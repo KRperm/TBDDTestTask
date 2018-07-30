6f213e3c69867ba81bf0120d1477c436f9f6cbe1
Hoork <HoorkGames@yandex.ru>
2018-07-30 23:16:26 +0500

    remade some parts of client and server

--- client.py
+++ client.py
@@ -1,26 +1,52 @@
-import sys
+#!/usr/bin/env python3
 import socket
 import select
-import re
-import json
 import datetime
+import json
+import pickle
+import argparse
+import re
+import sys
 
-class Client:
-	def __init__(self, recvBuffer = 4096, ipAddr = "127.0.0.1", port = 50000):
+class ChatClient:
+	def __init__(self, setup = {}):
+
+		clientProperties = {
+			"buffer": 4096, 
+			"ip": "127.0.0.1", 
+			"port": 50000
+		}
+		clientProperties.update(setup)
+
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.settimeout(2)
 
 		self.messageBuffer = ""
 
-		self.recvBuffer = recvBuffer
+		self.recvBuffer = clientProperties["buffer"]
 
 		self.socketList = [sys.stdin, self.sock]
-			 
-		try :
-			self.sock.connect((ipAddr, port))
-		except :
-			print('Не могу подключится')
-			sys.exit()
+
+		self.isConnectionRunning = True
+
+		self.errorMessages = [
+			"Вы должны быть авторизированы, чтобы выполнить это действие", #ERRORNOTAUTH = 0
+			"Вы уже авторизированы", #ERRORALREADYAUTH = 1
+			"Такого пользователя не существует или пароль введен неверно", #ERRORUSERNOTEXIST = 2
+			"Такой пользователь уже существует", #ERRORUSEREXIST = 3
+			"Войдите в чат-группу, чтобы выполнить это действие", #ERRORNOTINCHAT = 4
+			"Выйдите из чат-группы, чтобы выполнить это действие", #ERRORINCHAT = 5
+			"Неверное количество аргументов", #ERRORWRONGARGS = 6
+			"Не найдено сообщений за этот период" #ERRORHISTORYNOTFOUND = 7
+		]
+		self.successMessages = [
+			"вошел в учетную запись", #SUCCESSAUTH = 0
+			"зарегестрировался", #SUCCESSREG = 1
+			"вышел из учетной записи", #SUCCESSUNAUTH = 2
+			"вошел в группу", #SUCCESSENTER = 3
+			"вышел из группы", #SUCCESSQUIT = 4
+			"История сообщений получена" #SUCCESSGETHISTORY = 5
+		]
 
 		self.helpMessage = """-----
 Добро пожаловать
@@ -32,58 +58,116 @@ class Client:
 /quit - Выйти из чат-комнаты
 /history - Просмотр истории сообщений
 /disconn - Отключится от сервера и выйти из программы
------\n"""
+-----"""
+			 
+		try :
+			self.sock.connect((clientProperties["ip"], clientProperties["port"]))
+			print(self.helpMessage)
 
-		sys.stdout.write(self.helpMessage);
-		sys.stdout.flush()
-		self.loop()
+			if clientProperties.get("username"):
+				password = self.enterData("Введите пароль для пользователя %s" % clientProperties["username"])
+				if password:
+					self.sendMessage({"action": "auth", "name": clientProperties["username"], "password": password})
+		except :
+			print('Не могу подключится')
 
-	def loop(self):
-		while 1:
+	def __del__(self):
+		self.isConnectionRunning = False
+		self.sock.close()
+		print("Отключаюсь")
+
+	def run(self):
+		while self.isConnectionRunning:
 			readyToRead,readyToWrite,inError = select.select(self.socketList , [], [])
 			 
 			for s in readyToRead:
 				if s == self.sock:
 					data = s.recv(self.recvBuffer)
 					if data :
-						data = data.decode("utf8")
-						sys.stdout.write(data)
-						sys.stdout.flush()	
+
+						try:
+							data = pickle.loads(data)
+						except:
+							print("Сервер послал некорректные данные")
+							continue
+
+						self.parseServerMessage(data)
 					else :
 						print("Нет соединения с сервером")
-						sys.exit()
-				
+						self.isConnectionRunning = False;
 				else :
 					message = sys.stdin.readline()[:-1]
-					self.parseChat(message)
-					sys.stdout.flush()
+					self.parseInput(message)
 
-	def parseChat(self, message):
+	def getDescriptionOfServerResponse(self, data):
+		if(data.get("success") != None):
+			return self.successMessages[data["success"]]
+		elif(data.get("error") != None):
+			return "Ошибка. " + self.errorMessages[data["error"]]
+		else:
+			return None
+
+
+	def parseServerMessage(self, data):
+		serverResponse = self.getDescriptionOfServerResponse(data)
+		if serverResponse:
+			print(">>> " + serverResponse)
+
+		if(data.get("newmessage")):
+			print(self.composeMessage((data["time"], data["newmessage"], data["from"])))
+
+		if(data.get("historymessages")):
+			for message in data["historymessages"]:
+				print(self.composeMessage(message))
+
+	def parseInput(self, message):
 		if(message == "/help"):
-			sys.stdout.write(self.helpMessage)
+			print(self.helpMessage)
+
+
 		elif(message == "/reg"):
-			print("Для регистрации введите свой псевдоним и пароль") #Придумать что-то с этим
+			print("Для регистрации введите свой псевдоним и пароль")
 			namepass = self.enterNameAndPass();
 			if(namepass):
-				self.sendMessage("R " + namepass)
+				ObjectToSend = {"action": "reg"}
+				ObjectToSend.update(namepass)
+				self.sendMessage(ObjectToSend)
+
+
 		elif(message == "/auth"):
 			print("Для авторизации введите свой псевдоним и пароль")
 			namepass = self.enterNameAndPass();
 			if(namepass):
-				self.sendMessage("A " + namepass)
+				ObjectToSend = {"action": "auth"}
+				ObjectToSend.update(namepass)
+				self.sendMessage(ObjectToSend)
+
+		elif(message == "/unauth"):
+			self.sendMessage({"action": "unauth"})
+
 		elif(message == "/enter"):
-			self.sendMessage("E")
+			self.sendMessage({"action": "enter"})
+
+
 		elif(message == "/quit"):
-			self.sendMessage("Q")
+			self.sendMessage({"action": "quit"})
+
+
 		elif(message == "/disconn"):
-			self.sendMessage("D")
+			self.sendMessage({"action": "disconnect"})
+
+
 		elif(message == "/history"):
 			print("В каком временном периоде искать сообщения?")
 			date = self.enterFromTimeAndToTime();
 			if(date):
-				self.sendMessage("H " + date)
+				ObjectToSend = {"action": "gethistory"}
+				ObjectToSend.update(date)
+				self.sendMessage(ObjectToSend)
+
+
 		else:
-			self.sendMessage("M " + message)
+			self.sendMessage({"action": "broadcast", "message": message})
 
 	def enterFromTimeAndToTime(self):
 		timeFrom = self.enterDate("С какой даты нужно искать сообщения?")
@@ -94,7 +178,7 @@ class Client:
 		if(not timeTo):
 			print("Ввод данных отменен")
 			return None
-		return "{} {}".format(int(timeFrom.timestamp()), int(timeTo.timestamp()))
+		return  {"timeFrom": int(timeFrom.timestamp()), "timeTo": int(timeTo.timestamp())}
 
 	def enterDate(self, lable):
 		while True:
@@ -118,6 +202,7 @@ class Client:
 			inputData = input()
 			if(inputData == "/quit"):
 				return None
+
 			reResult = re.search("[a-zA-Z0-9]+", inputData)
 			if(reResult and reResult.group(0) == inputData):
 				return inputData
@@ -135,22 +220,51 @@ class Client:
 		if(not passwd):
 			print("Ввод данных отменен")
 			return None
-		return "{} {}".format(name, passwd)
+		return {"name": name, "password": passwd}
 
 	def sendMessage(self, message):
-		message = message.encode("utf8")
-		self.sock.send(message)
+		try:
+			messageToInput = pickle.dumps(message)
+		except:
+			print('Не могу сформировать сообщение')
+			return None
+
+		try:
+			self.sock.send(messageToInput)
+		except:
+			print("Соединение с сервером потеряно")
+
+	def composeMessage(self, data): # data = (time, message, username)
+		return "[%s] %s: %s" % (datetime.datetime.fromtimestamp(data[0]), data[2], data[1])
 
 
+if __name__ == '__main__':
+	setup = {}
+	client = None
+	try:
+		with open('clientSetup.json', 'r', encoding='utf-8') as jsonData:
+			setup = json.load(jsonData)
+	except:
+		print("Нет файла конфигурации. Использую стандартные значения")
 
-setup = None
-jsonData = None
-try:
-	jsonData = open('clientSetup.json', 'r', encoding='utf-8')
-except:
-	print("Нет файла конфигурации. Использую стандартные значения")
-	Client()
-	sys.exit()
+	parser = argparse.ArgumentParser(add_help=True)
+	parser.add_argument('-b', '--buffer', action='append', help='Buffer size of message that will be received', type=int)
+	parser.add_argument('-i', '--ip', action='append', help='Connect to chat server at this ip address')
+	parser.add_argument('-p', '--port', action='append', help='Connect to chat server with this port', type=int)
+	parser.add_argument('-u', '--username', action='append', help='Authorise under this username after program starts')
 
-setup = json.load(jsonData)
-Client(setup.get('buffer', 4096), setup.get('ip', '127.0.0.1'), setup.get('port', 50000))
+	args = vars(parser.parse_args())
+	setupFromArgs = {}
+	for key in args:
+		if args[key]:
+			setupFromArgs[key] = args[key][0]
+	setup.update(setupFromArgs)
+
+	try:
+		client = ChatClient(setup)
+		client.run()
+	except (KeyboardInterrupt, SystemExit):
+		del client
+	except Exception as e:
+		raise e
+		
--- server.py
+++ server.py
@@ -1,93 +1,126 @@
+#!/usr/bin/env python3
 import socket
 import select
-import sqlite3
 import datetime
 import json
+import pickle
+import argparse
+import sqlite3
 import time
 
 class Database:
+
 	def __init__(self, dbName):
+		INITSCRIPT = '''
+		CREATE TABLE IF NOT EXISTS "user" ("UserId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"UserName" TEXT NOT NULL,"UserPassword" TEXT NOT NULL);
+		CREATE TABLE IF NOT EXISTS "message" ("MessageId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"MessageTime" INTEGER NOT NULL,"MessageAuthor" INTEGER NOT NULL,"MessageContent" TEXT NOT NULL);
+		CREATE UNIQUE INDEX IF NOT EXISTS "MessageIdIndex" ON "message" ("MessageId");
+		CREATE UNIQUE INDEX IF NOT EXISTS "UserIdIndex" ON "user" ("UserId");
+		CREATE INDEX IF NOT EXISTS "MessageAuthorIndex" ON "message" ("MessageAuthor");
+		'''
+
 		self.connection = sqlite3.connect(dbName)
 		self.cursor = self.connection.cursor()
-		self.cursor.executescript('CREATE TABLE IF NOT EXISTS "User" ("UserId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"UserName" TEXT NOT NULL,"UserPassword" TEXT NOT NULL);CREATE TABLE IF NOT EXISTS "message" ("MessageId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"MessageTime" INTEGER NOT NULL,"MessageAuthor" INTEGER NOT NULL,"MessageContent" TEXT NOT NULL);')
+		self.cursor.executescript(INITSCRIPT)
 		self.connection.commit()
 
 	def addUser(self, name, password):
-		self.cursor.execute("INSERT INTO user (UserName, UserPassword) VALUES ('{}', '{}')".format(name, password))
+		self.cursor.execute("INSERT INTO user (UserName, UserPassword) VALUES (?, ?)", (name, password))
 		self.connection.commit()
 
 	def getUserByNameAndPass(self, name, password):
-		self.cursor.execute("SELECT * FROM user WHERE UserName = '{}' AND UserPassword = '{}'".format(name, password))
+		self.cursor.execute("SELECT * FROM user WHERE UserName = ? AND UserPassword = ?", (name, password))
 		return self.cursor.fetchone()
 
 	def getUserByName(self, name):
-		self.cursor.execute("SELECT * FROM user WHERE UserName = '{}'".format(name))
+		self.cursor.execute("SELECT * FROM user WHERE UserName = ?", (name,))
 		return self.cursor.fetchone()
 
 	def addMessage(self, author, message):
-		self.cursor.execute("INSERT INTO message (MessageTime, MessageAuthor, MessageContent) VALUES ({}, {}, '{}')".format( int(time.time()), author, message))
+		self.cursor.execute("INSERT INTO message (MessageTime, MessageAuthor, MessageContent) VALUES (?, ?, ?)", ( int(time.time()), author, message))
 		self.connection.commit()
 
 	def getMessageFromTime(self, timeFrom, timeTo):
-		self.cursor.execute("SELECT * FROM message INNER JOIN user on message.MessageAuthor = user.UserId WHERE MessageTime >= {} AND MessageTime <= {} ORDER BY MessageTime ASC".format(timeFrom, timeTo))
+		self.cursor.execute("SELECT MessageTime, MessageContent, UserName FROM message INNER JOIN user ON message.MessageAuthor = user.UserId WHERE MessageTime >= ? AND MessageTime <= ? ORDER BY MessageTime ASC", (timeFrom, timeTo))
 		return self.cursor.fetchall()
 
 class User:
-	def __init__(self, name, sock):
+	def __init__(self, name, sock, addr):
 		self.sock = sock
+		self.addr = addr
 		self.name = name
 		self.id = None
 		self.isInChatRoom = False
-		self.isAuth = False;
+		self.isAuth = False
+		print("+Client (%s, %s) connected" % addr)
+
+	def __del__(self):
+		print("-Client (%s, %s) disconnected" % self.addr)
+		self.sock.close();
 
 
 class ChatServer:
-	"""docstring for Server"""
-	def __init__(self, recvBuffer = 4096, ipAddr = "127.0.0.1", port = 50000, serverName = "Server", dbName = "chat.db"):
-		#super(Server, self).__init__()
+
+	def __init__(self, setup = {}):
+
+		serverProperties = {
+			"buffer": 4096, 
+			"ip": "127.0.0.1", 
+			"port": 50000, 
+			"servername": "Server", 
+			"dbname": "chat.db"
+		}
+		serverProperties.update(setup)
+
 		self.userList = [];
 
-		self.recvBuffer = recvBuffer
+		self.recvBuffer = serverProperties["buffer"]
 
-		serverSocket = socket.socket()
-		serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		serverSocket.bind((ipAddr, port))
-		serverSocket.listen(10)
-		self.serverAccount = User(serverName, serverSocket)
-		self.serverAccount.isInChatRoom = True
-		self.serverAccount.isAuth = True
+		self.isConnectionRunning = True
 
-		self.userList.append(self.serverAccount)
+		self.serverSocket = socket.socket()
+		self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.serverSocket.bind((serverProperties["ip"], serverProperties["port"]))
+		self.serverSocket.listen(10)
 
-		self.db = Database(dbName)
+		self.serverName = serverProperties["servername"]
 
-		self.loop()
+		self.db = Database(serverProperties["dbname"])
+
+	def __del__(self):
+		self.isConnectionRunning = False
+		for user in self.userList:
+			del user
+		self.serverSocket.close()
 
 	def getSocketsList(self):
-		return [u.sock for u in self.userList]
+		return [u.sock for u in self.userList] + [self.serverSocket]
 
-	def loop(self):
-		while True:
-			socketList = self.getSocketsList();
-			readyToRead,readyToWrite,inError = select.select(socketList,[],[],0)
+	def run(self):
+		print("starting server")
+		while self.isConnectionRunning:
+			readyToRead,readyToWrite,inError = select.select(self.getSocketsList(),[],[],0)
 			for s in readyToRead:
-				if s == self.serverAccount.sock:
-					sockfd, addr = self.serverAccount.sock.accept()
+				if s == self.serverSocket:
+					sockfd, addr = self.serverSocket.accept()
 
-					self.userList.append(User("Guest", sockfd))
-					print("Client (%s, %s) connected" % addr)
+					self.userList.append(User("Guest", sockfd, addr))
 				else:
 					data = s.recv(self.recvBuffer);
 					if data:
-						data = data.decode("utf8");
+						
+						try:
+							data = pickle.loads(data)
+						except:
+							continue
+
 						self.parseChat(data, self.findUserBySocket(s))
 					else:
 						discUser = self.findUserBySocket(s)
 						if(discUser.isInChatRoom):
-							self.broadcast("Соединение с {} было разорвано".format(discUser.name), self.serverAccount);
+							self.broadcast("Соединение с %s было разорвано" % discUser.name);
 						self.userList.remove(discUser)
-
-		serverAccount.sock.close()
+						del discUser
 
 
 	def findUserBySocket(self, sock):
@@ -97,127 +130,162 @@ class ChatServer:
 		return None
 
 	def parseChat(self, data, sender):
-		if data != "":
-			rawData = data[2:].replace("\r", "").replace("\n", "")
-			args = rawData.split(" ")
-
-			#L data1 data2 ...
-			if (data[0] == "R"): #register // R user password
-				if sender.isInChatRoom:
-					self.sendMessage("Выйдите из чат группы, чтобы пройти регистрацию", sender, self.serverAccount)
-				elif len(args) != 2:
-					self.sendMessage("Недостаточно агрументов. Ожидаю 2", sender, self.serverAccount)
-				else:
-					if(self.db.getUserByName(args[0]) == None):
-						self.db.addUser(args[0], args[1])
-						self.sendMessage("Пользователь {} успешно зарегистрировался".format(args[0]), sender, self.serverAccount)
-					else:
-						self.sendMessage("Пользователь с таким именем уже существует", sender, self.serverAccount)
-					
-			#######
-			elif (data[0] == "A"): #auth
-				if len(args) != 2:
-					self.sendMessage("Недостаточно агрументов. Ожидаю 2", sender, self.serverAccount)
-				elif sender.isAuth:
-					self.sendMessage("Уже авторизированы", sender, self.serverAccount)
-				else:
-					user = self.db.getUserByNameAndPass(args[0], args[1])
-					if(user != None):
-						sender.name = user[1]
-						sender.id = user[0]
-						sender.isAuth = True
-						self.sendMessage("Успешно авторизовался", sender, self.serverAccount)
-					else:
-						self.sendMessage("Пользователя не существует или пароль введен не верно", sender, self.serverAccount)
-						
-			#######
-			elif (data[0] == "U"): #unauth
-				if sender.isInChatRoom:
-					self.sendMessage("Не могу выйти из учетной записи. Выйдите из чат-группы сначала", sender, self.serverAccount)
-				else:
-					self.sendMessage("Вышел из учетной записи", sender, self.serverAccount)
-					sender.isAuth = False;
-			#######
-			elif (data[0] == "E"): #enter to room
-				if not sender.isAuth:
-					self.sendMessage("Для входа необходима авторизация", sender, self.serverAccount)
-				elif sender.isInChatRoom:
-					self.sendMessage("Вы уже в чат-группе", sender, self.serverAccount)
-				else:
-					self.sendMessage("Вы вошли в чат-группу", sender, self.serverAccount)
-					self.broadcast("{} подключился".format(sender.name), self.serverAccount)
-					sender.isInChatRoom = True;
-					
-			#######
-			elif (data[0] == "Q"): #quit from room
-				if sender.isInChatRoom:
-					sender.isInChatRoom = False;
-					self.sendMessage("Вы вышли из чат-группы", sender, self.serverAccount)
-					self.broadcast("{} вышел из чата".format(sender.name), self.serverAccount)
-				else:
-					self.sendMessage("Вы не находитесь в чат-группе", sender, self.serverAccount)
-			#######
-			elif (data[0] == "M"): #send message
-				if not sender.isAuth:
-					self.sendMessage("Для отправки сообщения необходима авторизация", sender, self.serverAccount)
-				elif not sender.isInChatRoom:
-					self.sendMessage("Войди в чат-группу", sender, self.serverAccount)
-				else:
-					self.db.addMessage(sender.id, rawData)
-					self.broadcast(rawData, sender)
-			#######
-			elif (data[0] == "D"): #Disconnect
-				if sender.isInChatRoom:
-					self.broadcast("{} вышел из чата.".format(sender.name), self.serverAccount);
-				sender.sock.close()
-				self.userList.remove(sender)
-			#######
-			elif (data[0] == "H"): #History
-				if len(args) != 2:
-					self.sendMessage("Недостаточно агрументов. Ожидаю 2", sender, self.serverAccount)
-				elif sender.isInChatRoom:
-					self.sendMessage("Выйдите из чат-грппы, чтобы посмотреть историю сообщений", sender, self.serverAccount)
-				else:
-					hMessages = self.db.getMessageFromTime(args[0], args[1])
-					if len(hMessages) > 0:
-						self.sendMessage("=== Результат запроса ===\n" + self.composeMessages(hMessages) + "=== Конец запроса ===", sender, self.serverAccount)
-					else:
-						self.sendMessage("Сообщений в этот период не найдено", sender, self.serverAccount)
-					
+		#const
+		ERRORNOTAUTH = 0
+		ERRORALREADYAUTH = 1
+		ERRORUSERNOTEXIST = 2
+		ERRORUSEREXIST = 3
+		ERRORNOTINCHAT = 4
+		ERRORINCHAT = 5
+		ERRORWRONGARGS = 6
+		ERRORHISTORYNOTFOUND = 7
 
 
-	def broadcast(self, message, sender):
+		SUCCESSAUTH = 0
+		SUCCESSREG = 1
+		SUCCESSUNAUTH = 2
+		SUCCESSENTER = 3
+		SUCCESSQUIT = 4
+		SUCCESSGETHISTORY = 5
+
+		if(data["action"] == "reg"):
+			if sender.isInChatRoom:
+				self.sendMessage({"error": ERRORINCHAT}, sender)
+			elif not(data.get("name") and data.get("password")):
+				self.sendMessage({"error": ERRORWRONGARGS}, sender)
+			elif (self.db.getUserByName(data["name"]) != None):
+				self.sendMessage({"error": ERRORUSEREXIST}, sender)
+			else:
+				self.db.addUser(data["name"], data["password"])
+				self.sendMessage({"success": SUCCESSREG}, sender)
+
+
+		elif(data["action"] == "auth"):
+			if sender.isAuth:
+				self.sendMessage({"error": ERRORALREADYAUTH}, sender)
+			elif not(data.get("name") and data.get("password")):
+				self.sendMessage({"error": ERRORWRONGARGS}, sender)
+			else:
+				user = self.db.getUserByNameAndPass(data["name"], data["password"])
+				if(user != None):
+					sender.name = user[1]
+					sender.id = user[0]
+					sender.isAuth = True
+					self.sendMessage({"success": SUCCESSAUTH}, sender)
+				else:
+					self.sendMessage({"error": ERRORUSERNOTEXIST}, sender)
+
+		elif(data["action"] == "unauth"):
+			if not sender.isAuth:
+				self.sendMessage({"error": ERRORNOTAUTH}, sender)
+			elif sender.isInChatRoom:
+				self.sendMessage({"error": ERRORINCHAT}, sender)
+			else:
+				self.sendMessage({"success": SUCCESSUNAUTH}, sender)
+				sender.isAuth = False;
+
+		elif(data["action"] == "enter"):
+			if not sender.isAuth:
+				self.sendMessage({"error": ERRORNOTAUTH}, sender)
+			elif sender.isInChatRoom:
+				self.sendMessage({"error": ERRORINCHAT}, sender)
+			else:
+				self.sendMessage({"success": SUCCESSENTER}, sender)
+				self.broadcast({"newmessage": "%s подключился." % sender.name, "from": self.serverName})
+				sender.isInChatRoom = True;
+
+
+		elif(data["action"] == "quit"):
+			if sender.isInChatRoom:
+				sender.isInChatRoom = False;
+				self.sendMessage({"success": SUCCESSQUIT}, sender)
+				self.broadcast({"newmessage": "%s вышел из чата." % sender.name, "from": self.serverName})
+			else:
+				self.sendMessage({"error": ERRORNOTINCHAT}, sender)
+
+
+		elif(data["action"] == "disconnect"):
+			if sender.isInChatRoom:
+				self.broadcast({"newmessage": "%s вышел из чата." % sender.name, "from": self.serverName})
+			self.userList.remove(sender)
+			del sender
+
+
+		elif(data["action"] == "gethistory"):
+			if not(data.get("timeFrom") and data.get("timeTo")):
+				self.sendMessage({"error": ERRORWRONGARGS}, sender)
+			elif sender.isInChatRoom:
+				self.sendMessage({"error": ERRORINCHAT}, sender)
+			else:
+				hMessages = self.db.getMessageFromTime(data["timeFrom"], data["timeTo"])
+				if len(hMessages) > 0:
+					self.sendMessage({"success": SUCCESSGETHISTORY, "historymessages": hMessages}, sender)
+				else:
+					self.sendMessage({"error": ERRORHISTORYNOTFOUND}, sender)
+
+
+		elif(data["action"] == "broadcast"):
+			if not sender.isAuth:
+				self.sendMessage({"error": ERRORNOTAUTH}, sender)
+			elif not sender.isInChatRoom:
+				self.sendMessage({"error": ERRORNOTINCHAT}, sender)
+			else:
+				self.db.addMessage(sender.id, data["message"])
+				self.broadcast({"newmessage": data["message"], "from": sender.name})
+
+
+	def broadcast(self, message):
+		message.update({"time": int(time.time())})
 		for u in self.userList:
-			if u.sock != self.serverAccount.sock and u.isInChatRoom and u.isAuth:
-				self.sendMessage(message, u, sender, False)
+			if u.isInChatRoom and u.isAuth:
+				self.sendMessage(message, u)
 				
 
-	def sendMessage(self, message, sendto, sendfrom, isPrivate = True):
-		privateSign = ""
-		if isPrivate:
-			privateSign = "(Личное сообщение)"
-		message = "[{}] {}{}: {}\n".format(datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S"), sendfrom.name, privateSign, message)
-		message = message.encode('utf8')
+	def sendMessage(self, message, sendto):
+		try:
+			messageToSend = pickle.dumps(message)
+		except:
+			print("Cannot encode data: %s" % message)
+			return None
+
 		try :
-			sendto.sock.send(message)
+			sendto.sock.send(messageToSend)
 		except :
 			discUser = self.findUserBySocket(s)
 			if(discUser.isInChatRoom):
-				self.broadcast("Соединение с {} было разорвано.".format(discUser.name), self.serverAccount);
-			sendto.sock.close()
+				self.broadcast({"newmessage": "Соединение с %s было разорвано." % discUser.name, "from": self.serverName});
 			self.userList.remove(discUser)
+			del discUser
 
-	def composeMessages(self, rawData):
-		result = ""
-		for row in rawData:
-			result += "[{}] {}: {}\n".format(datetime.datetime.fromtimestamp(row[1]), row[5], row[3])
-		return result
+if __name__ == '__main__':
+	setup = {}
+	server = None
 
-setup = None
-try:
-	with open('serverSetup.json', 'r', encoding='utf-8') as jsonData:
-		setup = json.load(jsonData)
-		ChatServer(setup.get('buffer', 4096), setup.get('ip', '127.0.0.1'), setup.get('port', 50000), setup.get('servername', 'Server'), setup.get('dbname', 'chat.db'));
-except:
-	print("no setup file")
-	ChatServer();
+	try:
+		with open('serverSetup.json', 'r', encoding='utf-8') as jsonData:
+			setup = json.load(jsonData)
+	except:
+		print("no setup file")
+
+	parser = argparse.ArgumentParser(add_help=True)
+	parser.add_argument('-b', '--buffer', action='append', help='Buffer size of message that will be received', type=int)
+	parser.add_argument('-i', '--ip', action='append', help='Ip Address of server')
+	parser.add_argument('-p', '--port', action='append', help='Port of server', type=int)
+	parser.add_argument('-n', '--servername', action='append', help='Name of server, which will shown to client')
+	parser.add_argument('-d', '--dbname', action='append', help='Name for database file')
+
+	args = vars(parser.parse_args())
+	setupFromArgs = {}
+	for key in args:
+		if args[key]:
+			setupFromArgs[key] = args[key][0]
+	setup.update(setupFromArgs)
+
+	try:
+		server = ChatServer(setup)
+		server.run()
+	except (KeyboardInterrupt, SystemExit):
+		print('shutting down the server')
+		del server
+	except Exception as e:
+		raise e
